16c9ce7738bb98a5f66d8f2301b3bbe7977b305b
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-02 18:35:05 -0300

    add basic components

---
+++ REFERENCE.md
@@ -0,0 +1,3 @@
+# Build a Responsive Website using Svelte in 30 minutes
+
+[Reference](https://dev.to/karkranikhil/build-responsive-website-using-svelte-in-30-minutes-l9)
---
+++ src/Components/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navbar from "./Navbar/Navbar.svelte";
+	import Banner from "./Banner/Banner.svelte";
+	import Footer from "./Footer/Footer.svelte";
+	
+	import DATA from "../Data/data.js";
+</script>
+
+<Navbar navlists={DATA.NAVBAR_DATA} header={DATA.HEADER} />
+<Banner bannerData={DATA.BANNER_DATA} />
+<Footer footerData={DATA.FOOTER_DATA} header={DATA.HEADER} />
