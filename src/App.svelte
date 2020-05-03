acdd63730925c380ef221053c3ac5c8ec59d1a0f
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-03 09:44:21 -0300

    Reactivity / Statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
 	let count = 0;
-	$: doubled = count * 2;
 
 	function handleClick() {
 		count += 1;
+	}
+
+	$: if (count >= 10) {
+		alert(`count is dangerously high!`);
+		count = 9;
 	}
 </script>
 
 <button on:click={handleClick}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
-
-<p>{count} doubled is {doubled}</p>
