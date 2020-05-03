69265cfa372cfa8e9be0a58164ae3bb48c5df9bf
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-03 10:00:02 -0300

    Reactivity / Updating arrays and objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,15 @@
 <script>
-	let count = 0;
+	let numbers = [1, 2, 3, 4];
 
-	function handleClick() {
-		count += 1;
+	function addNumber() {
+		numbers = [...numbers, numbers.length + 1];
 	}
 
-	$: if (count >= 10) {
-		alert(`count is dangerously high!`);
-		count = 9;
-	}
+	$: sum = numbers.reduce((t, n) => t + n, 0);
 </script>
 
-<button on:click={handleClick}>
-	Clicked {count} {count === 1 ? 'time' : 'times'}
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
 </button>
