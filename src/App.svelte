656431f59af70b6cc3260e186c2de3b4e1b38fd2
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-03 10:35:28 -0300

    Props / Spread props

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
-	import Nested from './Nested.svelte';
+	import Info from './Info.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	};
 </script>
 
-<Nested answer={42}/>
-<Nested/>
+<Info {...pkg}/>
