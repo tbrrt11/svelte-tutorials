0e106f3cd7386b975107c2f9a497b4c123ea3787
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-06 16:26:20 -0300

    Bindings / Select multiple

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,53 @@
 <script>
-	let questions = [
-		{ id: 1, text: `Where did you go to school?` },
-		{ id: 2, text: `What is your mother's name?` },
-		{ id: 3, text: `What is another personal fact that an attacker could easily find with Google?` }
+	let scoops = 1;
+	let flavours = ['Mint choc chip'];
+
+	let menu = [
+		'Cookies and cream',
+		'Mint choc chip',
+		'Raspberry ripple'
 	];
 
-	let selected;
-
-	let answer = '';
-
-	function handleSubmit() {
-		alert(`answered question ${selected.id} (${selected.text}) with "${answer}"`);
+	function join(flavours) {
+		if (flavours.length === 1) return flavours[0];
+		return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
 	}
 </script>
 
-<style>
-	input { display: block; width: 500px; max-width: 100%; }
-</style>
+<h2>Size</h2>
 
-<h2>Insecurity questions</h2>
+<label>
+	<input type=radio bind:group={scoops} value={1}>
+	One scoop
+</label>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<select bind:value={selected} on:change="{() => answer = ''}">
-		{#each questions as question}
-			<option value={question}>
-				{question.text}
-			</option>
-		{/each}
-	</select>
+<label>
+	<input type=radio bind:group={scoops} value={2}>
+	Two scoops
+</label>
 
-	<input bind:value={answer}>
-	
-	<button disabled={!answer} type=submit>
-		Submit
-	</button>
-</form>
+<label>
+	<input type=radio bind:group={scoops} value={3}>
+	Three scoops
+</label>
 
-<p>selected question {selected ? selected.id : '[waiting...]'}</p>
+<h2>Flavours</h2>
+
+<select multiple bind:value={flavours}>
+	{#each menu as flavour}
+		<option value={flavour}>
+			{flavour}
+		</option>
+	{/each}
+</select>
+
+{#if flavours.length === 0}
+	<p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+	<p>Can't order more flavours than scoops!</p>
+{:else}
+	<p>
+		You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+		of {join(flavours)}
+	</p>
+{/if}
