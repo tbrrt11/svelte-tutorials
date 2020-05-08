f9869a2e5dede650e82daa4bb70791f930213c76
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-08 12:45:07 -0300

    Lifecycle / onDestroy

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,11 @@
 <script>
-	import { onMount } from 'svelte';
+    import { onInterval } from './utils.js';
 
-	let photos = [];
-
-	onMount(async () => {
-		const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=20`);
-		photos = await res.json();
-	});
+    let seconds = 0;
+    const interval = onInterval(() => seconds += 1, 1000);
 </script>
 
-<style>
-	.photos {
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-gap: 8px;
-	}
-
-	figure, img {
-		width: 100%;
-		margin: 0;
-	}
-</style>
-
-<h1>Photo Album</h1>
-
-<div class="photos">
-	{#each photos as photo}
-		<figure>
-			<img src={photo.thumbnailUrl} alt={photo.title}>
-			<figcaption>{photo.title}</figcaption>
-		</figure>
-	{:else}
-		<p>loading...</p>
-	{/each}
-</div>
+<p>
+    The page has been open for
+    {seconds} {seconds === 1 ? 'second' : 'seconds'}
+</p>
