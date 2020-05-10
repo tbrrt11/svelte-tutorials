e8c945d6d5655c2a1b7d63e3ed99b2d8625afbe7
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-09 21:39:19 -0300

    Lifecycle / tick

--- src/App.svelte
+++ src/App.svelte
@@ -1,109 +1,37 @@
 <script>
-    import Eliza from 'elizabot';
-    import { beforeUpdate, afterUpdate } from 'svelte';
+    import { tick } from 'svelte';
 
-    let div;
-    let autoscroll;
+    let text = `Select some text and hit the tab key to toggle uppercase`;
 
-    beforeUpdate(() => {
-        autoscroll = div && (div.offsetHeight + div.scrollTop) > (div.scrollHeight - 20);
-    });
+    async function handleKeydown(event) {
+        if (event.key !== 'Tab') return;
+        
+        event.preventDefault();
 
-    afterUpdate(() => {
-        if (autoscroll) div.scrollTo(0, div.scrollHeight);
-    });
+        const { selectionStart, selectionEnd, value } = this;
+        const selection = value.slice(selectionStart, selectionEnd);
 
-    const eliza = new Eliza();
+        const replacement = /[a-z]/.test(selection)
+            ? selection.toUpperCase()
+            : selection.toLowerCase();
 
-    let comments = [
-        { author: 'eliza', text: eliza.getInitial() }
-    ];
+        text = (
+            value.slice(0, selectionStart) +
+            replacement +
+            value.slice(selectionEnd)
+        );
 
-    function handleKeydown(event) {
-        if (event.key === 'Enter') {
-            const text = event.target.value;
-            if (!text) return;
-
-            comments = comments.concat({
-                author: 'user',
-                text
-            });
-
-            event.target.value = '';
-
-            const reply = eliza.transform(text);
-
-            setTimeout(() => {
-                comments = comments.concat({
-                    author: 'eliza',
-                    text: '...',
-                    placeholder: true
-                });
-
-                setTimeout(() => {
-                    comments = comments
-                        .filter(comment => !comment.placeholder)
-                        .concat({
-                            author: 'eliza',
-                            text: reply
-                        })
-                }, 500 + Math.random() * 500);
-            }, 200 + Math.random() * 200);
-        }
+        await tick();
+        this.selectionStart = selectionStart;
+        this.selectionEnd = selectionEnd;
     }
 </script>
 
 <style>
-    .chat {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        max-width: 320px;
-    }
-
-    .scrollable {
-        flex: 1 1 auto;
-        border-top: 1px solid #eee;
-        margin: 0 0 0.5rem 0;
-        overflow-y: auto;
-    }
-
-    article {
-        margin: 0.5rem 0;
-    }
-
-    .user {
-        text-align: right;
-    }
-
-    span {
-        padding: 0.5em 1em;
-        display: inline-block;
-    }
-
-    .eliza span {
-        background-color: #eee;
-        border-radius: 1em 1em 1em 0;
-    }
-
-    .user span {
-        background-color: #0074D9;
-        color: white;
-        border-radius: 1em 1em 0 1em;
-        word-break: break-all;
+    textarea {
+        width: 100%;
+        height: 200px;
     }
 </style>
 
-<div class="chat">
-    <h1>Eliza</h1>
-
-    <div class="scrollable" bind:this={div}>
-        {#each comments as comment}
-            <article class={comment.author}>
-                <span>{comment.text}</span>
-            </article>
-        {/each}
-    </div>
-
-    <input on:keydown={handleKeydown}>
-</div>
+<textarea value={text} on:keydown={handleKeydown}></textarea>
