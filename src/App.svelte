63348d3c80f1226714ee5fee329178f87b37217a
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-16 17:04:16 -0300

    Stores / Readable stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-    import { count } from './store.js';
-    import Incrementer from './Incrementer.svelte';
-    import Decrementer from './Decrementer.svelte';
-    import Resetter from './Resetter.svelte';
+    import { time } from './stores.js';
+
+    const formatter = new Intl.DateTimeFormat('en', {
+        hour12: true,
+        hour: 'numeric',
+        minute: '2-digit',
+        second: '2-digit'
+    });
 </script>
 
-<h1>The count is {$count}</h1>
-
-<Incrementer/>
-<Decrementer/>
-<Resetter/>
+<h1>The time is {formatter.format($time)}</h1>
