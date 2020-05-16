600e275d01874ccc134b10a0edeebe22eac0d3d6
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-16 16:52:31 -0300

    Stores / Writable stores

---
+++ src/Incrementer.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { count } from './store.js';
+
+    function increment() {
+        count.update(n => n + 1);
+    }
+</script>
+
+<button on:click={increment}>
+    +
+</button>
