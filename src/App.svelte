e0cc3c9e380d275417a2c40e996c5f425016b9a6
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-05-16 16:54:27 -0300

    Stores / Auto-subscriptions

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,9 @@
     import Incrementer from './Incrementer.svelte';
     import Decrementer from './Decrementer.svelte';
     import Resetter from './Resetter.svelte';
-
-    let count_value;
-
-    const unsubscribe = count.subscribe(value => {
-        count_value = value;
-    });
 </script>
 
-<h1>The count is {count_value}</h1>
+<h1>The count is {$count}</h1>
 
 <Incrementer/>
 <Decrementer/>
