dc254175d30537a03c06cfd48d8dca7cc9ee5f38
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-07-12 21:51:24 -0300

    Transitions / Transition events

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,24 @@
 <script>
-    let visible = false;
+    import { fly } from 'svelte/transition';
 
-    function typewriter(node, { speed = 50 }) {
-        const valid = (
-            node.childNodes.length === 1 &&
-            node.childNodes[0].nodeType === Node.TEXT_NODE
-        );
-
-        if (!valid) {
-            throw new Error(`This transition only works on elements with a single text node child`);
-        }
-
-        const text = node.textContent;
-        const duration = text.length * speed;
-
-        return {
-            duration,
-            tick: t => {
-                const i = ~~(text.length * t);
-                node.textContent = text.slice(0, i);
-            }
-        }
-    }
+    let visible = true;
+    let status = 'waiting...';
 </script>
+
+<p>status: {status}</p>
 
 <label>
     <input type="checkbox" bind:checked={visible}>
 </label>
 
 {#if visible}
-    <p in:typewriter={{ speed: 200 }}>
-        The quick brown fox jumps over the lazy dog
+    <p
+        transition:fly="{{ y: 200, duration: 2000 }}"
+        on:introstart="{() => status = 'intro started'}"
+        on:outrostart="{() => status = 'outro started'}"
+        on:introend="{() => status = 'intro ended'}"
+        on:outroend="{() => status = 'outro ended'}"
+    >
+        Flies in and out
     </p>
 {/if}
