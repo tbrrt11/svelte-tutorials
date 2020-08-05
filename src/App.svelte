3e7dc8ebc97d73d65b76c2dc265cea508d47ae47
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-08-05 20:56:34 -0300

    Component composition / Slots

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,8 @@
 <script>
-    let big = false;
+    import Box from './Box.svelte';
 </script>
 
-<style>
-    .big {
-        font-size: 4em;
-    }
-</style>
-
-<label>
-    <input type=checkbox bind:checked={big}>
-    big
-</label>
-
-<div class:big>
-    some {big ? 'big': 'small'} text
-</div>
+<Box>
+    <h2>Hello!</h2>
+    <p>This is a box. It can contain anything.</p>
+</Box>
