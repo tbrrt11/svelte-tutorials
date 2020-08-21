d5aabb5ebf13d6678dc31f14e684619b2a9be1a7
tbrrt11 <32571922+tbrrt11@users.noreply.github.com>
2020-08-21 06:30:54 -0300

    Special elements / <svelte:self>

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,41 @@
 <script>
-	import Map from './Map.svelte';
-	import MapMarker from './MapMarker.svelte';
+	import Folder from './Folder.svelte';
+
+	let root = [
+		{
+			type: 'folder',
+			name: 'Important work stuff',
+			files: [
+				{ type: 'file', name: 'quarterly-results.xlsx' }
+			]
+		},
+		{
+			type: 'folder',
+			name: 'Animal GIFs',
+			files: [
+				{
+					type: 'folder',
+					name: 'Dogs',
+					files: [
+						{ type: 'file', name: 'treadmill.gif' },
+						{ type: 'file', name: 'rope-jumping.gif' }
+					]
+				},
+				{
+					type: 'folder',
+					name: 'Goats',
+					files: [
+						{ type: 'file', name: 'parkour.gif' },
+						{ type: 'file', name: 'rampage.gif' }
+					]
+				},
+				{ type: 'file', name: 'cat-roomba.gif' },
+				{ type: 'file', name: 'duck-shuffle.gif' },
+				{ type: 'file', name: 'monkey-on-a-pig.gif' }
+			]
+		},
+		{ type: 'file', name: 'TODO.md' }
+	];
 </script>
 
-<Map lat={35} lon={-84} zoom={3.5}>
-	<MapMarker lat={37.8225} lon={-122.0024} label="Svelte Body Shaping"/>
-	<MapMarker lat={33.8981} lon={-118.4169} label="Svelte Barbershop & Essentials"/>
-	<MapMarker lat={29.7230} lon={-95.4189} label="Svelte Waxing Studio"/>
-	<MapMarker lat={28.3378} lon={-81.3966} label="Svelte 30 Nutritional Consultants"/>
-	<MapMarker lat={40.6483} lon={-74.0237} label="Svelte Brands LLC"/>
-	<MapMarker lat={40.6986} lon={-74.4100} label="Svelte Medical Systems"/>
-</Map>
+<Folder name="Home" files={root} expanded/>
--- src/mapbox.js
+++
@@ -1,7 +0,0 @@
-import mapbox from 'mapbox-gl';
-
-mapbox.accessToken = 'pk.eyJ1IjoidGJycnQxMSIsImEiOiJjanZ1djdtaHozdmozNDhxb3BtNWtqcjRpIn0.vbdJu_79Sa9yxcBFdrBbsw';
-
-const key = {};
-
-export { mapbox, key };
